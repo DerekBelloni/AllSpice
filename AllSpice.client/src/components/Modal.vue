4380e23859d1da6a0bd5c34b0f19be04e8a06c47
Derek <derekbelloni10@gmail.com>
2022-04-11 09:29:18 -0600

    commit

--- AllSpice.client/src/components/Modal.vue
+++ AllSpice.client/src/components/Modal.vue
@@ -7,7 +7,7 @@
     aria-labelledby="modelTitleId"
     aria-hidden="true"
   >
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-xl" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">
